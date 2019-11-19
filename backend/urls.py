ca2ac9cdb00aa21b3fb15b2dae26e4563ba19a2c
TaeHun Lee <dlrytnwkd@naver.com>
2019-11-19 19:41:16 +0900

    Change API

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PostViewSet
-
-router = routers.DefaultRouter()
-router.register('posts', PostViewSet)
+from .views import PostListAPI, PostCreateAPI, PostDetailAPI, PostUpdateAPI, PostDeleteAPI
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', PostListAPI.as_view()),
+    path('create/', PostCreateAPI.as_view()),
+    path('detail/<int:pk>', PostDetailAPI.as_view()),
+    path('update/<int:pk>', PostUpdateAPI.as_view()),
+    path('delete/<int:pk>', PostDeleteAPI.as_view()),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from .serializers import PostSerializer
 from .models import Post
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostListAPI(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostCreateAPI(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailAPI(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostUpdateAPI(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDeleteAPI(generics.DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
